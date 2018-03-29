24a86a022406dbbc29281ca0a5cee11f7c8bb9d9
Kasper Henningsen <kasperkh.kh@gmail.com>
2018-03-29 23:59:54 +0200

    Made global.js inline, removed SoundCloud API wrapper

--- docs/global.js
+++ docs/global.js
@@ -1,4 +1,46 @@
+window.xhr = (reqContent, url, options = {}, callback) => {
+    return new Promise((resolve, reject) => {
+        if (typeof options == "function") callback = options;
+        if (typeof options == "function") options = {};
+        if (options.type == undefined)        options.type = "POST";
+        if (options.contentType == undefined) options.contentType = "json";
+        var xhr = new XMLHttpRequest();
+        xhr.open(options.type, url, true);
+        if (options.type == "GET") {
+            xhr.send();
+        } else if (options.contentType == "form") {
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.send("data="+JSON.stringify(reqContent));
+        } else if (options.contentType == "json") {
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.send(JSON.stringify(reqContent));
+        } else if (options.contentType == "none") {
+            xhr.send(reqContent);
+            // for file uploads (multipart/form-data)
+        } else if (options.contentType) {
+            xhr.setRequestHeader("Content-type", options.contentType);
+            xhr.send(reqContent);
+        }
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                let res = JSON.parse(this.responseText);
+                let err = null;
+                if (!String(this.status).startsWith("2")) {
+                    console.error("HTTP error "+this.status);
+                    err = this.status;
+                }
+                if (err) reject(err);
+                else resolve(res);
+                if (callback) callback(res, err);
+            }
+        };
+        // resolve(5);
+    });
+}
+
 $(document).ready(() => {
+
+
 
     // YouTube API
     function start() {
@@ -77,11 +119,9 @@ $(document).ready(() => {
     gapi.load("client", start);
 
     // SoundCloud API
-    SC.initialize({
-    	client_id: "6ibYZTmF5qnpvp88S9V3werVrC18WCdC" //change
-    });
-    SC.get("/users/247370320/tracks", {limit: 6}).then(function(tracks) {
-        console.log(tracks);
+    var clientID = "6ibYZTmF5qnpvp88S9V3werVrC18WCdC";
+    var url = "http://api.soundcloud.com/users/247370320/tracks?limit=6&client_id="+clientID;
+    xhr(null, url, {type: "GET"}).then((tracks) => {
         for (var i = 0; i < tracks.length; i++) {
             var url = tracks[i].permalink_url;
             var img = tracks[i].artwork_url.replace("large", "t300x300");
@@ -90,6 +130,9 @@ $(document).ready(() => {
             item.find("img").attr("src", img);
         }
         $("section.lacuna").removeClass("hidden");
+    }, (err) => {
+        throw Error("Could not fetch SoundCloud tracks");
+        console.log(err);
     });
 
     // discord/email popups
