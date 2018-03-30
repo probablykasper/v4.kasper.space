5d3a727d088d12020484c10a03b1d0833f674900
Kasper Henningsen <kasperkh.kh@gmail.com>
2018-03-30 23:09:24 +0200

    Removing YouTube Data API wrapper, starting removal of jQuery

--- docs/global.js
+++ docs/global.js
@@ -37,91 +37,87 @@ window.xhr = (reqContent, url, options = {}, callback) => {
         // resolve(5);
     });
 }
+var YTapiKey = "AIzaSyBnQnpboWUfWyR8aW6HuQV5MAlxZ5FQ090";
+var maxResults = 6;
+
+(function Video() {
+
+    var url = "https://www.googleapis.com/youtube/v3/playlistItems"
+    +"?key="+YTapiKey
+    +"&part=snippet"
+    +"&maxResults="+maxResults
+    +"&playlistId=";
+    var personalURL = url+"UUy6jcAF6fZGttRvihyQixbA";
+    var commissionsURL = url+"PL84-DNDSU8p5WP6jA7hvOCV9dIKBNjdCS";
+    var personal = xhr(null, personalURL, {type: "GET"});
+    var commissions = xhr(null, commissionsURL, {type: "GET"});
+    Promise.all([personal, commissions]).then((results) => {
+        console.log("==--==--==--> YouTube Personal + Commissions");
+        console.log(results);
+        // combine video and commission responses
+        var items = results[0].items.concat(results[1].items);
+        items.sort(function(a, b) {
+            var dateA = new Date(a.snippet.publishedAt);
+            var dateB = new Date(b.snippet.publishedAt);
+            return dateB - dateA; // sort by date ascending
+        });
+        items = items.splice(0, maxResults);
+        for (var i = 0; i < items.length; i++) {
+            var url = items[i].snippet.resourceId.videoId;
+            var img = items[i].snippet.thumbnails.high.url;
+            var title = items[i].snippet.title;
+            var item = document.querySelector("section.video .thumbnails a.item:nth-child("+(i+1)+")");
+            item.setAttribute("href", "https://www.youtube.com/watch?v="+url);
+            item.querySelector("img").setAttribute("src", img);
+        }
+        document.querySelector("section.video").classList.remove("hidden");
+
+    }, (err) => {
+        throw Error("Could not fetch YouTube playlistItems");
+        console.log(err);
+    });
+
+})();
+
+(function Synctan() {
+
+    var url = "https://www.googleapis.com/youtube/v3/playlistItems"
+    +"?key="+YTapiKey
+    +"&part=snippet"
+    +"&maxResults="+maxResults
+    +"&playlistId=UUoMimPRPeR28TM3_0cinjVQ";
+    xhr(null, url, {type: "GET"}).then((result) => {
+        console.log("==--==--==-->YouTube Synctan");
+        console.log(result);
+        var items = result.items;
+        for (var i = 0; i < items.length; i++) {
+            var url = items[i].snippet.resourceId.videoId;
+            var img = items[i].snippet.thumbnails.high.url;
+            var title = items[i].snippet.title;
+            var item = document.querySelector("section.synctan .thumbnails a.item:nth-child("+(i+1)+")");
+            item.setAttribute("href", "https://www.youtube.com/watch?v="+url);
+            item.querySelector("img").setAttribute("src", img);
+        }
+        document.querySelector("section.synctan").classList.remove("hidden");
+    }, (err) => {
+        throw Error("Could not fetch YouTube playlistItems");
+        console.log(err);
+    });
+
+})();
 
 $(document).ready(() => {
 
+(function Lacuna() {
 
-
-    // YouTube API
-    function start() {
-    	gapi.client.init({
-    		"apiKey": "AIzaSyBnQnpboWUfWyR8aW6HuQV5MAlxZ5FQ090",
-    		"discoveryDocs": ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
-    	}).then(function() {
-
-    		// SYNCTAN
-    		// API request details
-    		var requestSynctan = gapi.client.youtube.playlistItems.list({
-    			part: "snippet",
-    			playlistId: "UUoMimPRPeR28TM3_0cinjVQ",
-    			maxResults: 6
-    		});
-    		// Perform API request, process response
-    		requestSynctan.execute(function(response) {
-                var items = response.result.items;
-                for (var i = 0; i < items.length; i++) {
-                    var url = items[i].snippet.resourceId.videoId;
-                    var img = items[i].snippet.thumbnails.high.url;
-                    var title = items[i].snippet.title;
-                    var item = $("section.synctan .thumbnails a.item:nth-child("+(i+1)+")");
-                    item.attr("href", "https://www.youtube.com/watch?v="+url);
-                    item.find("img").attr("src", img);
-                }
-                $("section.synctan").removeClass("hidden");
-    		});
-
-    		// VIDEO (personal)
-    		// API request details
-    		var requestVideo = gapi.client.youtube.playlistItems.list({
-    			part: "snippet",
-    			playlistId: "UUy6jcAF6fZGttRvihyQixbA",
-    			maxResults: 6
-    		});
-    		var requestCommissions = gapi.client.youtube.playlistItems.list({
-    			part: "snippet",
-    			playlistId: "PL84-DNDSU8p5WP6jA7hvOCV9dIKBNjdCS",
-    			maxResults: 6
-    		});
-    		// Perform API request, process response
-            var videoResponse, commissionResponse;
-    		requestVideo.execute(function(response) {
-                videoResponse = response;
-                processResponses();
-    		});
-            requestCommissions.execute(function(response) {
-                commissionResponse = response;
-                processResponses();
-    		});
-            function processResponses() {
-                if (videoResponse && commissionResponse) {
-                    // combine video and commission responses
-                    items = videoResponse.result.items.concat(commissionResponse.result.items);
-                    items.sort(function(a, b) {
-                        var dateA = new Date(a.snippet.publishedAt);
-                        var dateB = new Date(b.snippet.publishedAt);
-                        return dateB - dateA; // sort by date ascending
-                    });
-                    items = items.splice(0, 6);
-                    for (var i = 0; i < items.length; i++) {
-                        var url = items[i].snippet.resourceId.videoId;
-                        var img = items[i].snippet.thumbnails.high.url;
-                        var title = items[i].snippet.title;
-                        var item = $("section.video .thumbnails a.item:nth-child("+(i+1)+")");
-                        item.attr("href", "https://www.youtube.com/watch?v="+url);
-                        item.find("img").attr("src", img);
-                    }
-                    $("section.video").removeClass("hidden");
-                }
-            }
-
-    	});
-    }
-    gapi.load("client", start);
-
-    // SoundCloud API
+    // Lacuna
     var clientID = "6ibYZTmF5qnpvp88S9V3werVrC18WCdC";
-    var url = "http://api.soundcloud.com/users/247370320/tracks?limit=6&client_id="+clientID;
+    var url = "http://api.soundcloud.com/users/247370320/tracks"
+        +"?limit="+maxResults
+        +"&client_id="+clientID;
     xhr(null, url, {type: "GET"}).then((tracks) => {
+        console.log("==--==--==--> SoundCloud Lacuna");
+        console.log(tracks);
         for (var i = 0; i < tracks.length; i++) {
             var url = tracks[i].permalink_url;
             var img = tracks[i].artwork_url.replace("large", "t300x300");
@@ -135,23 +131,25 @@ $(document).ready(() => {
         console.log(err);
     });
 
-    // discord/email popups
-    $(document).on("click", function(e) {
+})();
 
-        if ($(e.target).hasClass("has-popup") || $(e.target).hasClass("popup")) {
-            if ($(e.target).find("input").hasClass("hidden")) {
-                // hidden
-                $("a.has-popup input").addClass("hidden");
-                $(e.target).find("input").select().removeClass("hidden");
-            } else {
-                // not hidden
-                $(e.target).find("input").addClass("hidden");
-            }
-        } else {
-            // clicked anywhere
+// discord/email popups
+$(document).on("click", function(e) {
+
+    if ($(e.target).hasClass("has-popup") || $(e.target).hasClass("popup")) {
+        if ($(e.target).find("input").hasClass("hidden")) {
+            // hidden
             $("a.has-popup input").addClass("hidden");
+            $(e.target).find("input").select().removeClass("hidden");
+        } else {
+            // not hidden
+            $(e.target).find("input").addClass("hidden");
         }
+    } else {
+        // clicked anywhere
+        $("a.has-popup input").addClass("hidden");
+    }
 
-    });
+});
 
 });
