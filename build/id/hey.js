e98ecf9ad403065a0c1daf49bf53cd4a02b90cdb
Kasper <kasperkh.kh@gmail.com>
2018-10-14 18:14:22 +0200

    Initial build subtree commit

---
+++ build/id/hey.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log(function () {
+    return 55;
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImlkL2hleS5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiOztBQUFBQSxRQUFRQyxHQUFSLENBQVksWUFBTTtBQUNkLFdBQU8sRUFBUDtBQUNILENBRkQiLCJmaWxlIjoiaWQvaGV5LmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coKCkgPT4ge1xuICAgIHJldHVybiA1NTtcbn0pIl19
