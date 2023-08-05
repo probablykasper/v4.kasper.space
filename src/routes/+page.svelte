6f86534a91b1e2cc751bd2e2d14039b919e13f8d
Kasper <kasperkh.kh@gmail.com>
2023-08-05 13:47:27 +0200

    Remove target="_blank"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,6 @@
 <div class="social-media">
 	<a
 		class="svg-link twitter"
-		target="_blank"
 		rel="noopener noreferrer"
 		title="Twitter"
 		href="https://twitter.com/probablykasper"
@@ -38,7 +37,6 @@
 	</div>
 	<a
 		class="svg-link youtube"
-		target="_blank"
 		rel="noopener noreferrer"
 		title="YouTube"
 		href="https://youtube.com/probablykasper"
@@ -51,7 +49,6 @@
 	</a>
 	<a
 		class="instagram svg-link"
-		target="_blank"
 		rel="noopener noreferrer"
 		title="Instagram"
 		href="https://www.instagram.com/kasper.space/"
@@ -67,7 +64,6 @@
 	</a>
 	<a
 		class="github svg-link"
-		target="_blank"
 		rel="noopener noreferrer"
 		title="GitHub"
 		href="https://github.com/probablykasper"
@@ -80,7 +76,6 @@
 	</a>
 	<a
 		class="facebook svg-link"
-		target="_blank"
 		rel="noopener noreferrer"
 		title="Facebook"
 		href="https://www.facebook.com/kasper.space"
@@ -100,7 +95,6 @@
 	</div>
 	<a
 		class="soundcloud svg-link"
-		target="_blank"
 		rel="noopener noreferrer"
 		title="SoundCloud"
 		href="https://soundcloud.com/probablykasper"
@@ -123,7 +117,6 @@
 			<a
 				href="https://youtube.com/watch?v={video.id}"
 				class="item"
-				target="_blank"
 				rel="noopener noreferrer"
 				title={video.title}
 			>
@@ -133,7 +126,6 @@
 	</div>
 	<a
 		class="see-more"
-		target="_blank"
 		rel="noopener noreferrer"
 		href="https://www.youtube.com/playlist?list=PL84-DNDSU8p5WP6jA7hvOCV9dIKBNjdCS"
 	>
@@ -151,7 +143,6 @@
 			<a
 				href="https://youtube.com/watch?v={video.id}"
 				class="item"
-				target="_blank"
 				rel="noopener noreferrer"
 				title={video.title}
 			>
@@ -159,12 +150,7 @@
 			</a>
 		{/each}
 	</div>
-	<a
-		class="see-more"
-		target="_blank"
-		rel="noopener noreferrer"
-		href="https://www.youtube.com/shrive"
-	>
+	<a class="see-more" rel="noopener noreferrer" href="https://www.youtube.com/shrive">
 		<p>See more</p>
 	</a>
 </section>
@@ -175,28 +161,27 @@
 		<p class="description">I am a co-owner of this independent indie record label.</p>
 	</div>
 	<div class="thumbnails">
-		<a class="item" target="_blank" rel="noopener noreferrer">
+		<a class="item" rel="noopener noreferrer">
 			<img alt="" />
 		</a>
-		<a class="item" target="_blank" rel="noopener noreferrer">
+		<a class="item" rel="noopener noreferrer">
 			<img alt="" />
 		</a>
-		<a class="item" target="_blank" rel="noopener noreferrer">
+		<a class="item" rel="noopener noreferrer">
 			<img alt="" />
 		</a>
-		<a class="item" target="_blank" rel="noopener noreferrer">
+		<a class="item" rel="noopener noreferrer">
 			<img alt="" />
 		</a>
-		<a class="item" target="_blank" rel="noopener noreferrer">
+		<a class="item" rel="noopener noreferrer">
 			<img alt="" />
 		</a>
-		<a class="item" target="_blank" rel="noopener noreferrer">
+		<a class="item" rel="noopener noreferrer">
 			<img alt="" />
 		</a>
 	</div>
 	<a
 		class="see-more"
-		target="_blank"
 		rel="noopener noreferrer"
 		href="https://soundcloud.com/lacunarecs"
 	>
@@ -222,7 +207,6 @@
 							href={repo.homepageUrl}
 							class="repo-website svg-link"
 							title="Website"
-							target="_blank"
 							rel="noopener noreferrer"
 						>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
@@ -233,13 +217,7 @@
 							</svg>
 						</a>
 					{/if}
-					<a
-						href={repo.url}
-						class="repo-url svg-link"
-						title="Repository"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+					<a href={repo.url} class="repo-url svg-link" title="Repository" rel="noopener noreferrer">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
 							<path
 								d="M16 .395c-8.836 0-16 7.163-16 16 0 7.069 4.585 13.067 10.942 15.182.8.148 1.094-.347 1.094-.77 0-.381-.015-1.642-.022-2.979-4.452.968-5.391-1.888-5.391-1.888-.728-1.849-1.776-2.341-1.776-2.341-1.452-.993.11-.973.11-.973 1.606.113 2.452 1.649 2.452 1.649 1.427 2.446 3.743 1.739 4.656 1.33.143-1.034.558-1.74 1.016-2.14-3.554-.404-7.29-1.777-7.29-7.907 0-1.747.625-3.174 1.649-4.295-.166-.403-.714-2.03.155-4.234 0 0 1.344-.43 4.401 1.64a15.353 15.353 0 014.005-.539c1.359.006 2.729.184 4.008.539 3.054-2.07 4.395-1.64 4.395-1.64.871 2.204.323 3.831.157 4.234 1.026 1.12 1.647 2.548 1.647 4.295 0 6.145-3.743 7.498-7.306 7.895.574.497 1.085 1.47 1.085 2.963 0 2.141-.019 3.864-.019 4.391 0 .426.288.925 1.099.768C27.421 29.457 32 23.462 32 16.395c0-8.837-7.164-16-16-16z"
@@ -250,12 +228,7 @@
 			</div>
 		{/each}
 	</div>
-	<a
-		class="see-more"
-		target="_blank"
-		rel="noopener noreferrer"
-		href="https://github.com/probablykasper"
-	>
+	<a class="see-more" rel="noopener noreferrer" href="https://github.com/probablykasper">
 		<p>See more</p>
 	</a>
 </section>
